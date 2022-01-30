e49b181aeabc316f1d6df103c15c48128392ce8f
David Baek <davidbaek04@gmail.com>
2022-01-29 19:17:41 -0500

    update

---
+++ data-12-3-2-resources/spaceX.js
@@ -0,0 +1,14 @@
+
+// API call to SpaceX
+// const url = "https://api.spacexdata.com/v2/launchpads";
+// d3.json(url).then(receivedData => console.log(receivedData));
+
+// const url = "https://api.spacexdata.com/v2/launchpads";
+// d3.json(url).then(data => console.log(data));
+// d3.json(url).then(spaceXResults => console.log(spaceXResults[0]));
+// d3.json(url).then(spaceXResults => console.log(spaceXResults[0].full_name));
+// d3.json(url).then(spaceXResults => console.log(spaceXResults[0].location.latitude));
+
+d3.json("samples.json").then(function(data){
+    console.log("hello");
+});
